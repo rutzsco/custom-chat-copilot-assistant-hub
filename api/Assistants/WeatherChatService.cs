702b1a50dc4f1f85366380433b3c37b4f97c8877
Scott Rutz <scrutz@microsoft.com>
2025-01-28 13:15:28 -0600

    Update WeatherChatService.cs

--- api/Assistants/WeatherChatService.cs
+++ api/Assistants/WeatherChatService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using Assistants.API.Core;
 using Assistants.API.Services.Prompts;
@@ -28,50 +29,7 @@ internal sealed class WeatherChatService
         _configuration = configuration;
     }
 
-    public async Task<ChatChunkResponse> ReplyStaticAsync(ChatTurn[] chatMessages, CancellationToken cancellationToken = default)
-    {
-        var sw = Stopwatch.StartNew();
-
-        // Kernel setup
-        var kernel = _openAIClientFacade.GetKernel(false);
-       
-
-        var s0 = kernel.Plugins.GetFunction("Weather", "GetLocation");
-        var s1 = kernel.Plugins.GetFunction("Weather", "GetLocationLatLong");
-        var s2 = kernel.Plugins.GetFunction("Weather", "GetForcast");
-
-        var context = new KernelArguments();
-        context["chatMessages"] = chatMessages;
-        context["question"] = chatMessages.LastOrDefault()?.User;
-
-        // RAG Steps
-        await kernel.InvokeAsync(s0, context);
-        await kernel.InvokeAsync(s1, context);
-        await kernel.InvokeAsync(s2, context);
-
-        // Chat Step
-        var chatGpt = kernel.Services.GetService<IChatCompletionService>();
-
-
-        var chatHistory = new ChatHistory(PromptService.GetPromptByName("WeatherChatSystemPrompt"));
-        var userMessage = await PromptService.RenderPromptAsync(kernel, PromptService.GetPromptByName("WeatherChatUserPrompt"), context);
-        chatHistory.AddUserMessage(userMessage);
-
-        var sb = new StringBuilder();
-        await foreach (StreamingChatMessageContent chatUpdate in chatGpt.GetStreamingChatMessageContentsAsync(chatHistory, DefaultSettings.AIChatRequestSettings, null, cancellationToken))
-        {
-            if (chatUpdate.Content != null)
-            {
-                await Task.Delay(1);
-                sb.Append(chatUpdate.Content);
-            }
-        }
-        sw.Stop();
-
-        return new ChatChunkResponse(sb.ToString(), null);
-    }
-
-    public async IAsyncEnumerable<ChatChunkResponse> ReplyPlannerAsync(ChatTurn[] chatMessages, CancellationToken cancellationToken = default)
+    public async IAsyncEnumerable<ChatChunkResponse> ReplyPlannerAsync(ChatTurn[] chatMessages, [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
         var sw = Stopwatch.StartNew();
 
